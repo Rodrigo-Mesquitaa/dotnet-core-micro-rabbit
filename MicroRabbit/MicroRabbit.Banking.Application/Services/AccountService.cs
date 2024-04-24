c062331e6bcb3c6060bdb77a3e40220e4b686c46
unknown <rodrigo09.mesquita@gmail.com>
2024-04-24 00:04:12 -0300

    projeto micro + rabbitmq

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,46 @@
+﻿
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Banking.Application.Interfaces;
+using System.Collections.Generic;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Domain.Core.Bus;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    /**
+     * Account Service class
+     * 
+     * @author D. P. Edwards
+     * @license MIT
+     * @version 1.0
+     */
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _bus;
+
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
+        {
+            _accountRepository = accountRepository;
+            _bus = bus;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                accountTransfer.FromAccount,
+                accountTransfer.ToAccount,
+                accountTransfer.TransferAmount
+            );
+
+            _bus.SendCommand(createTransferCommand);
+        }
+    }
+}
